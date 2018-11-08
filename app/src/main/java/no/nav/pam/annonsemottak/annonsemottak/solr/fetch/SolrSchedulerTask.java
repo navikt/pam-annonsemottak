d8325cc09c3e33367af847ac289e8be30f384cd9
eziztm <eziztm@yahoo.com>
2018-11-08 11:01:03 +0100

    mergers stopping ads into the same task

--- app/src/main/java/no/nav/pam/annonsemottak/annonsemottak/solr/fetch/SolrSchedulerTask.java
+++ app/src/main/java/no/nav/pam/annonsemottak/annonsemottak/solr/fetch/SolrSchedulerTask.java
@@ -1,6 +1,7 @@
 package no.nav.pam.annonsemottak.annonsemottak.solr.fetch;
 
 import net.javacrumbs.shedlock.core.SchedulerLock;
+import no.nav.pam.annonsemottak.stilling.Stilling;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("prod")
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
@@ -16,17 +19,21 @@ public class SolrSchedulerTask {
 
     private static final Logger LOG = LoggerFactory.getLogger(SolrSchedulerTask.class);
     private final SolrFetchService solrFetchService;
+    private final StopSolrStillingerService stopSolrStillingerService;
 
     @Autowired
-    SolrSchedulerTask(SolrFetchService solrFetchService) {
+    SolrSchedulerTask(SolrFetchService solrFetchService,
+                      StopSolrStillingerService stopSolrStillingerService) {
         this.solrFetchService = solrFetchService;
+        this.stopSolrStillingerService = stopSolrStillingerService;
     }
 
     @Scheduled(cron = "0 0 8 * * *")
     @SchedulerLock(name = "saveLatestAdsFromStillingsolr")
     public void saveLatestAdsFromStillingsolr() {
         try {
-            solrFetchService.saveStillingerFromSolr();
+            List<Stilling> savedAds = solrFetchService.saveStillingerFromSolr();
+            stopSolrStillingerService.findAndStopOldSolrStillinger(savedAds);
 
             LOG.info("Scheduler for stillingsolr ferdig");
         } catch (Exception e) {
