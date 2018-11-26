b43bae337c65c8687cf0c6e311231d65a328862b
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2018-11-26 16:50:04 +0100

    added metrics config

---
+++ app/src/main/java/no/nav/pam/annonsemottak/app/config/MetricsConfig.java
@@ -0,0 +1,36 @@
+package no.nav.pam.annonsemottak.app.config;
+
+import io.micrometer.core.instrument.Tag;
+import org.slf4j.Logger;
+import org.springframework.boot.actuate.metrics.web.client.RestTemplateExchangeTags;
+import org.springframework.boot.actuate.metrics.web.client.RestTemplateExchangeTagsProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.util.Arrays;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Configuration
+public class MetricsConfig {
+
+    private static final Logger LOG = getLogger(MetricsConfig.class);
+    @Bean
+    public RestTemplateExchangeTagsProvider restTemplateTagConfigurer() {
+        LOG.info("Limiting tags in rest template");
+        return new CustomRestTemplateExchangeTagsProvider();
+    }
+
+    private static class CustomRestTemplateExchangeTagsProvider implements RestTemplateExchangeTagsProvider {
+        @Override
+        public Iterable<Tag> getTags(String urlTemplate, org.springframework.http.HttpRequest request, ClientHttpResponse response) {
+            // we only use path for tags, because of hitting a limit of tags. The cardinality for uri might cause issue.
+            return Arrays.asList(
+                    RestTemplateExchangeTags.method(request),
+                    RestTemplateExchangeTags.uri(request.getURI().getPath()),
+                    RestTemplateExchangeTags.status(response),
+                    RestTemplateExchangeTags.clientName(request));
+        }
+    }
+}
