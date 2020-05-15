d9b176e573a33958cbcbeb43e1b2302d89e7689b
Tore Engvig <Tore.Engvig@nav.no>
2020-05-15 11:24:32 +0200

    Unngå å konvertere guillemets til <<

--- src/main/java/no/nav/pam/annonsemottak/markdown/HtmlToMarkdownConverter.java
+++ src/main/java/no/nav/pam/annonsemottak/markdown/HtmlToMarkdownConverter.java
@@ -5,14 +5,20 @@ import com.vladsch.flexmark.util.options.DataHolder;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 import org.apache.commons.text.StringEscapeUtils;
 
+import static com.vladsch.flexmark.convert.html.FlexmarkHtmlParser.TYPOGRAPHIC_QUOTES;
+
 /**
  * One parser, one configuration, same HTML-to-markdown behaviour for all sources.
  */
 public class HtmlToMarkdownConverter {
 
     // Set options here, see https://goo.gl/2xedeq for sample.
-    private static final DataHolder OPTIONS = new MutableDataSet();
+    private static final MutableDataSet OPTIONS = new MutableDataSet();
     private static final int MAX_BLANK_LINES = 2;
+
+    static {
+        OPTIONS.set(TYPOGRAPHIC_QUOTES, false);
+    }
 
     public static String parse(String html) {
         if (html == null) {
