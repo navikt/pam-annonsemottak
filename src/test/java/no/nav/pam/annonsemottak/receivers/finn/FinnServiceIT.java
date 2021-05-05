49ac7e636fc4d92975c94c8699c980458222b902
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-05-05 11:52:05 +0200

    trigger workflow

--- src/test/java/no/nav/pam/annonsemottak/receivers/finn/FinnServiceIT.java
+++ src/test/java/no/nav/pam/annonsemottak/receivers/finn/FinnServiceIT.java
@@ -2,5 +2,12 @@ package no.nav.pam.annonsemottak.receivers.finn;
 // Good when debug integration against FINN
 
 
+import org.junit.jupiter.api.Test;
+
 public class FinnServiceIT {
+
+    @Test
+    public void start() {
+        System.out.print("testing");
+    }
 }
