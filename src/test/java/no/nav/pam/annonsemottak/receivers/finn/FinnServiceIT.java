c0395c7c104a7beb665066dfdbf757d1eba1d61a
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-05-06 11:34:09 +0200

    finn integration test

--- src/test/java/no/nav/pam/annonsemottak/receivers/finn/FinnServiceIT.java
+++ src/test/java/no/nav/pam/annonsemottak/receivers/finn/FinnServiceIT.java
@@ -1,14 +1,15 @@
 package no.nav.pam.annonsemottak.receivers.finn;
-// Good when debug integration against FINN
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.pam.annonsemottak.Application;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.io.File;
 import java.util.Set;
 
 import static no.nav.pam.annonsemottak.receivers.finn.FinnService.KNOWN_COLLECTIONS;
@@ -24,7 +25,8 @@ public class FinnServiceIT {
 
     @Test
     public void start() throws Exception {
+        // Good when debug integration against FINN
         Set<FinnAdHead> finnAdHeads = finnConnector.fetchSearchResult(KNOWN_COLLECTIONS);
-        objectMapper.writeValueAsString(finnAdHeads);
+        //FileUtils.write(new File("finnAdHeads.json"), objectMapper.writeValueAsString(finnAdHeads));
     }
 }
