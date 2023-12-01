bb8fcb396d791363ca608f540bcbd902b92eb828
Christoffer Karlsson <christoffer.karlsson@nav.no>
2023-12-01 15:54:42 +0100

    Allow <section> tag with id attribute when sanitizing HTML

--- src/main/java/no/nav/pam/annonsemottak/markdown/HtmlSanitizer.java
+++ src/main/java/no/nav/pam/annonsemottak/markdown/HtmlSanitizer.java
@@ -1,16 +1,23 @@
 package no.nav.pam.annonsemottak.markdown;
 
+import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 
 public class HtmlSanitizer {
 
     public static String sanitize(String html){
+        PolicyFactory allowSectionTags = new HtmlPolicyBuilder()
+                .allowElements("section")
+                .allowAttributes("id").onElements("section")
+                .toFactory();
+
         PolicyFactory policy = Sanitizers.FORMATTING
                 .and(Sanitizers.LINKS)
                 .and(Sanitizers.BLOCKS)
                 .and(Sanitizers.TABLES)
-                .and(Sanitizers.FORMATTING);
+                .and(Sanitizers.FORMATTING)
+                .and(allowSectionTags);
 
         return policy.sanitize(html);
     }
